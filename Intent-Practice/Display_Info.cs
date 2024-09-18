a7fae0cf5a9c1f80d5a79d8e170fbf8082417320
Maxim Zyuryaev <maxim@Maxims-MacBook-Pro.local>
2024-09-18 15:28:15 +0300

    added activity and the option to transfer name and age to the other screen

---
+++ Intent-Practice/Display_Info.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Intent_Practice
+{
+	[Activity (Label = "Display_Info")]			
+	public class Display_Info : Activity
+	{
+		TextView Greeting, Age;
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+			SetContentView(Resource.Layout.display_info);
+			// Create your application here
+			Init();
+			Greeting.Text += Intent.GetStringExtra("Username") ?? "Guest";
+            Age.Text += Intent.GetStringExtra("Age") ?? "0";
+        }
+
+        void Init()
+		{
+			Greeting = FindViewById<TextView>(Resource.Id.greeting);
+			Age = FindViewById<TextView>(Resource.Id.AgeInfo);
+		}
+	}
+}
+
--- Intent-Practice/MainActivity.cs
+++ Intent-Practice/MainActivity.cs
@@ -2,18 +2,47 @@
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
+using Android.Widget;
+using System;
+using Android.Content;
 
 namespace Intent_Practice
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        Button Next;
+        EditText Age, Username;
+
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            Init();
+            SetClicks();
+        }
+
+        void SetClicks()
+        {
+            Next.Click += Next_Click;
+        }
+
+        private void Next_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(Display_Info));
+            intent.PutExtra("Age", Age.Text);
+            intent.PutExtra("Username", Username.Text);
+            StartActivity(intent);
+        }
+
+        void Init()
+        {
+            Next = FindViewById<Button>(Resource.Id.Next);
+            Age = FindViewById<EditText>(Resource.Id.Age);
+            Username = FindViewById<EditText>(Resource.Id.Username);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
